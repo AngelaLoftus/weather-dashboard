3efce17fdbc9e33600b4a569ca21a59ae2e46f33
Angela Loftus <angelaloftus@yahoo.com>
2021-11-01 21:13:34 -0500

    five day history

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,37 @@ var inputValue = document.querySelector(".inputValue");
 var cityName = document.querySelector(".cityName");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
+
+var temp1 = document.getElementById("temp1"); 
+var temp2 = document.getElementById("temp2"); 
+var temp3 = document.getElementById("temp3"); 
+var temp4 = document.getElementById("temp4"); 
+var temp5 = document.getElementById("temp5"); 
+
 var humidity = document.querySelector(".humidity");
+var humidity1 = document.getElementById("humidity1");
+var humidity2 = document.getElementById("humidity2");
+var humidity3 = document.getElementById("humidity3");
+var humidity4 = document.getElementById("humidity4");
+var humidity5 = document.getElementById("humidity5");
+
 var windspeed = document.getElementById("windspeed");
+var windspeed1 = document.getElementById("windspeed1");
+var windspeed2 = document.getElementById("windspeed2");
+var windspeed3 = document.getElementById("windspeed3");
+var windspeed4 = document.getElementById("windspeed4");
+var windspeed5 = document.getElementById("windspeed5");
+
 var icon = document.getElementById("icon");
+var icon1 = document.getElementById("icon1");
+var icon2 = document.getElementById("icon2");
+var icon3 = document.getElementById("icon3");
+var icon4 = document.getElementById("icon4");
+var icon5 = document.getElementById("icon5");
+
+
+var date1 = document.getElementById("date1");
+
 var historyEl = document.getElementById("history");
 let searchHistory = [];
 let lat; 
@@ -48,20 +76,53 @@ function fetchFiveDayWeather() {
             return(response.json());
         }).then(function(data) { 
             console.log(data);
-            returnFiveDayForecast();
+            returnFiveDayForecast(data);
         })
         .catch(function (error){
             console.log(error);
         });
 };
- 
-function returnFiveDayForecast() {
-        //get dates;
-        //heading (date itself);
-        //temperature
-        //wind
-        //humidity
-        //icon
+
+function returnFiveDayForecast(data) {
+        console.log("DATA", data);
+
+            //repeat this for all five days
+            //
+            var temp1Value = data.list[0].main.temp;
+            console.log(temp1Value);
+
+            var humidity1Value = data.list[0].main.humidity;
+            console.log(humidity1Value);
+            
+            var windspeedValue1 = data.list[0].wind.speed;
+            console.log(windspeedValue1);
+            
+            let weatherIcon1 = data.list[0].weather[0].icon;
+            console.log(weatherIcon1);
+            
+            var date1Value = data.list[0].dt_txt.split(" ")[0];
+            console.log(date1Value);
+
+
+            icon1.setAttribute( "src", "https://openweathermap.org/img/wn/" + weatherIcon1 + "@2x.png");
+            
+            temp1.innerHTML = Math.floor((temp1Value-273.15)*(9/5) + 32);
+            humidity1.innerHTML = humidity1Value;
+            windspeed1.innerHTML = windspeedValue1;
+            date1.innerHTML = date1Value;
+             // let weatherIcon = data.weather[0].icon;
+            // lat = data.coord.lat;
+            // console.log("lat", lat);
+            // long = data.coord.lon;
+            // console.log("long", long);
+            // console.log("icon", data.weather[0].icon); 
+            
+            // cityName.innerHTML= 'City Name: ' + nameValue + "(" + month + "/" + day + "/" + year + ")";
+            // temp.innerHTML = 'Temperature: ' + tempValue + "&degF;";
+            // desc.innerHTML = 'Description: ' + descValue;
+            // humidity.innerHTML = 'Humidity: ' + humidityValue + "%";
+            // windspeed.innerHTML = 'Wind Speed: ' + windspeedValue + " MPH";
+            // icon.setAttribute( "src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
 }
 
 //render UV index function for day 1
@@ -92,8 +153,6 @@ let geoApiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + inputValue.
 
 
 function handleSearchData () {
-
-
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + "&appid=" + apiKey + "&units=imperial")
         .then(response => response.json())
@@ -128,11 +187,17 @@ function handleSearchData () {
 })
 };
 
+button.addEventListener('click', handleSearchData);
 
+function handleSearchClick(e) {
+    let button = e.target;
+    let searchCity = button.getAttribute("data-search");
+    console.log(searchCity);
+    inputValue.value = searchCity;
+    handleSearchData();
 
-
-button.addEventListener('click',  handleSearchData);
-
+}
+historyEl.addEventListener('click',  handleSearchClick);
 // function handleSearchHistoryClick() {
 //     //we will work on this next time! 
 // }
